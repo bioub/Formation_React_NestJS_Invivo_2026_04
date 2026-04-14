d422cccfaed4476386f7db8a6157176b77ca9a30
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2026-04-14 15:40:25 +0200

    Refactor App component to implement a Todo list, removing unused elements and adding a form for new todos.

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,121 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
+import TodoItem from './TodoItem';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const todos = [
+    { id: '123abc', title: 'ABC', completed: true },
+    { id: '456def', title: 'DEF', completed: false },
+    { id: '789xyz', title: 'XYZ', completed: true },
+  ];
+  const editingId = '789xyz';
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <form className="todos-form">
+        <input type="checkbox" className="todos-toggle-checked" />
+        <input type="text" className="todos-new-input" />
+        <button>+</button>
+      </form>
+      <div className="todos-container">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} isEditing={editingId === todo.id} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ hello-react/src/TodoItem.tsx
@@ -0,0 +1,25 @@
+import type { Todo } from "./model";
+
+interface TodoItemProps {
+  readonly todo: Todo;
+  readonly isEditing: boolean;
+}
+
+function TodoItem({ todo, isEditing }: TodoItemProps) {
+  return (
+    <div className="todosItem" data-todo-id={todo.id}>      <input
+        type="checkbox"
+        className="todosCompleted"
+        checked={todo.completed}
+      />
+      {isEditing ? (
+        <input type="text" className="todosInputValue" value={todo.title} />
+      ) : (
+        <span className="todosSpanValue">{todo.title}</span>
+      )}
+      <button className="todosDeleteBtn">-</button>
+    </div>
+  );
+}
+
+export default TodoItem;
